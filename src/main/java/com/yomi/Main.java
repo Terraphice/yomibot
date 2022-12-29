c6cfa258b513ee4194d384318b01b57968983d31
Notarin Steele <424c414e4b@gmail.com>
2022-12-29 07:03:20 -0500

    Implemented basic Discord bot functionality

--- src/main/java/com/yomi/Main.java
+++ src/main/java/com/yomi/Main.java
@@ -1,9 +1,12 @@
 package com.yomi;
 
+import com.yomi.bot.DiscordBotHandler;
+
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Map<String, Object> config = ConfigHandler.loadConfig();
+        DiscordBotHandler.init(config);
     }
 }
---
+++ src/main/java/com/yomi/bot/Events.java
@@ -0,0 +1,7 @@
+package com.yomi.bot;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Events extends ListenerAdapter {
+
+}
