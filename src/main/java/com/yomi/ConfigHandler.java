c683a58cc4a401c764ed927d437a70ca15b291c9
Notarin Steele <424c414e4b@gmail.com>
2022-12-30 01:56:38 -0500

    Handled file not found exception properly

--- src/main/java/com/yomi/ConfigHandler.java
+++ src/main/java/com/yomi/ConfigHandler.java
@@ -12,7 +12,7 @@ import java.util.Map;
 public class ConfigHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigHandler.class);
     public static Map<String, Object> loadConfig() {
-        Map<String, Object> config;
+        Map<String, Object> config = null;
 
         try {
             //Loading config file
@@ -21,7 +21,11 @@ public class ConfigHandler {
             //set the config object
             config = yaml.load(inputStream);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            if (LOGGER.isErrorEnabled()) {
+                LOGGER.error("config.yml is not found! " +
+                        "Please copy config-example.yml to config.yml");
+            }
+            System.exit(1);
         }
 
         //check for any updates or problems to the bots config
@@ -31,7 +35,7 @@ public class ConfigHandler {
     }
 
     public static void updateCheck(final Map<String, Object> config) {
-        Map<String, Object> exampleConfig;
+        Map<String, Object> exampleConfig = null;
 
         try {
             //Loading config example file
@@ -40,16 +44,21 @@ public class ConfigHandler {
             //set the config object
             exampleConfig = yaml.load(inputStream);
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            if (LOGGER.isErrorEnabled()) {
+                LOGGER.error("config-example.yml is not found! " +
+                        "This should not happen unless you deleted it. " +
+                        "It is needed for update checks, please put it back");
+            }
+            System.exit(1);
         }
 
         //check for un-configured values in config, or useless values in config
         for (final Map.Entry<String, Object> configIteration : config.entrySet()) {
             //Check for keys in config not present in example config, this isn't really a problem so just warn.
-            if ((exampleConfig.get(configIteration.getKey()) == null) && (LOGGER.isWarnEnabled())) {
+            if (exampleConfig.get(configIteration.getKey()) == null && LOGGER.isWarnEnabled()) {
                 LOGGER.warn("Key found in config not present in example config: " + configIteration.getKey());
             }
-            if ((configIteration.getValue().equals(exampleConfig.get(configIteration.getKey()))) && (LOGGER.isErrorEnabled())) {
+            if (configIteration.getValue().equals(exampleConfig.get(configIteration.getKey())) && LOGGER.isErrorEnabled()) {
                 LOGGER.error("Config value unchanged, please configure key \"" + configIteration.getKey() + "\"");
                 System.exit(1);
             }
@@ -57,7 +66,7 @@ public class ConfigHandler {
 
         //check for missing keys in config
         for (final Map.Entry<String, Object> exampleConfigI : exampleConfig.entrySet()) {
-            if ((config.get(exampleConfigI.getKey()) == null) && (LOGGER.isErrorEnabled())) {
+            if (config.get(exampleConfigI.getKey()) == null && LOGGER.isErrorEnabled()) {
                 LOGGER.error("Key found in example config not present in config: " + exampleConfigI.getKey() +
                         "\nPlease copy all missing values and set them.");
                 System.exit(1);
