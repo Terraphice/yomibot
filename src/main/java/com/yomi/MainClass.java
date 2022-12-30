eb8897c8068601b88fe26c2a6c71a6fac8634b47
Notarin Steele <424c414e4b@gmail.com>
2022-12-30 01:37:35 -0500

    Renamed Main to MainClass

--- src/main/java/com/yomi/MainClass.java
+++ src/main/java/com/yomi/MainClass.java
@@ -4,7 +4,7 @@ import com.yomi.bot.DiscordBotHandler;
 
 import java.util.Map;
 
-public class Main {
+public class MainClass {
     public static void main(final String[] args) {
         final Map<String, Object> config = ConfigHandler.loadConfig();
         DiscordBotHandler.init(config);
