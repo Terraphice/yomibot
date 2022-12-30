7dd6edc0c58078d07ce48c00f7baf008a18874fb
Notarin Steele <424c414e4b@gmail.com>
2022-12-30 00:48:59 -0500

    Added temporary event for printing messages

--- src/main/java/com/yomi/bot/Events.java
+++ src/main/java/com/yomi/bot/Events.java
@@ -1,7 +1,16 @@
 package com.yomi.bot;
 
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Events extends ListenerAdapter {
-
+    public static final Logger LOGGER = LoggerFactory.getLogger(Events.class);
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        if (LOGGER.isInfoEnabled()) {
+            LOGGER.info(event.getMessage().getContentDisplay());
+        }
+    }
 }
