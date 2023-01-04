1d933af2f41ce88a24ee4c0e08dc213fde75a418
Notarin Steele <424c414e4b@gmail.com>
2023-01-04 06:01:42 -0500

    Fixed missing gateway intent

--- src/main/java/com/yomi/bot/DiscordBotHandler.java
+++ src/main/java/com/yomi/bot/DiscordBotHandler.java
@@ -2,6 +2,7 @@ package com.yomi.bot;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import java.util.Map;
 
@@ -9,6 +10,7 @@ public class DiscordBotHandler {
     public static void init(final Map<String, Object> config) {
         final String token = (String) config.get("token");
         final JDABuilder jdabuilder = JDABuilder.createDefault(token)
+                .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .addEventListeners(new Events());
         final JDA jda = jdabuilder.build();
         jda.addEventListener(new Events());
